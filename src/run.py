7d6179ef5eb78f53497813af2b86d9b86eed9abb
Max Vogel <max.vogel2@student.kit.edu>
2025-06-20 09:11:26 +0200

    Set model path for MQTPredictor dynamically

--- src/run.py
+++ src/run.py
@@ -1,6 +1,8 @@
 from api import create_app
+from selector import MqtPredictor
 
 app = create_app()
+MqtPredictor.set_model_path(r"..\..\training_env\mqt-predictor\src")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/selector/mqt_predictor.py
+++ src/selector/mqt_predictor.py
@@ -1,10 +1,6 @@
-TRAINED_MODEL_LOCATION = r"..\..\training_env\mqt-predictor\src"
-
 import sys
 
-sys.path.insert(0, TRAINED_MODEL_LOCATION)
-
-import mqt.predictor
+# mqt.predictor imported in MqtPredictor.set_model_path() to set the model path dynamically
 import mqt.bench
 from qiskit import QuantumCircuit
 import qiskit.qasm3
@@ -14,7 +10,16 @@ import util
 
 
 class MqtPredictor(Selector):
-    def select_device(self, openqasm_circuit: str):
+    @classmethod
+    def set_model_path(cls, model_path: str):
+        sys.path.insert(0, model_path)
+        cls.model = __import__("mqt.predictor", fromlist=["predictor"])
+
+    @classmethod
+    def select_device(cls, openqasm_circuit: str):
+        if not hasattr(cls, "model"):
+            raise ValueError("Model path not set. Call set_model_path() before using this method.")
+
         match util.get_openqasm_version(openqasm_circuit):
             case 2:
                 loaded_circuit = qiskit.qasm2.loads(openqasm_circuit)
@@ -23,7 +28,7 @@ class MqtPredictor(Selector):
             case _:
                 raise ValueError("Unsupported OpenQASM version")
 
-        compiled_circuit, compilation_information, quantum_device = mqt.predictor.qcompile(
+        compiled_circuit, compilation_information, quantum_device = cls.model.qcompile(
             loaded_circuit, figure_of_merit="expected_fidelity"
         )
 
