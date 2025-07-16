e7823bd0389a5a43b409a3e4ef1da32d1142a676
Max Vogel <max.vogel2@student.kit.edu>
2025-07-16 10:08:55 +0200

    Change path of MQT Predictor model

--- src/run.py
+++ src/run.py
@@ -1,9 +1,11 @@
 from api import create_app
 from selector import MqtPredictor
 
+MODEL_PATH = r"../data/model/mqt-predictor/src"
+
 app = create_app()
 
-MqtPredictor.set_model_path(r"..\..\training_env\mqt-predictor\src")
+MqtPredictor.set_model_path(MODEL_PATH)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False, use_evalex=False)
